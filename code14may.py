5325e94be6729983be454ec598f3572555555fa9
RheaL13 <lalr@stu.otc.school.nz>
2025-05-14 11:15:11 +1200

    code end of day

--- code14may.py
+++ code14may.py
@@ -395,6 +395,95 @@ class SignUpWindow:
         self.open_dashboard()
 
 
+# # All the code for the literacy area question
+# class LiteracyQuestion:
+#     """The code for the literacy inquiry question."""
+#     def __init__(self, parent):
+#         """Basic code for the display of the question window."""
+#         self.parent=parent
+#         self.question_window=tk.Toplevel()
+#         self.question_window.title("Question Window")
+#         self.question_window.configure(bg="oldlace")
+#         self.question_window.resizable(False, False)
+
+#         # Calculate center position
+#         w_w=600  # Window width
+#         w_h=320  # Window height
+#         screen_width=self.question_window.winfo_screenwidth()
+#         screen_height=self.question_window.winfo_screenheight()
+
+#         x_place=(screen_width-w_w)//2
+#         y_place=(screen_height-w_h)//4
+
+#         # Set window geometry
+#         self.question_window.geometry(f"{w_w}x{w_h}+{x_place}+{y_place}")
+
+#         # Headings for the different entries
+#         title=tk.Label(self.question_window, text="Questions", 
+#                        font=("Helvetica", 20, "bold"), 
+#                        fg="black", bg="oldlace")
+#         title.pack(pady=20)
+
+#         title1=tk.Label(self.question_window, text="First Name", 
+#                         font=("Helvetica", 13, "bold"), 
+#                         fg="black", bg="oldlace")
+#         title1.pack(pady=2)
+
+#         self.first_name=self.create_entry("First Name")
+
+#         title2=tk.Label(self.question_window, text="Username", 
+#                         font=("Helvetica", 13, "bold"), 
+#                         fg="black", bg="oldlace")
+#         title2.pack(pady=2)
+
+#         self.username=self.create_entry("Username")
+
+#     def create_entry(self, placeholder, show=None):
+#         """Display what the entry is for."""
+#         entry=tk.Entry(self.signup_window, show=show, 
+#                        font=("Helvetica", 12))
+#         entry.pack(pady=10)
+#         entry.insert(0, placeholder)
+#         entry.bind("<FocusIn>", lambda event, e=entry, 
+#                    p=placeholder:self.on_entry_click(event, e, p))
+#         entry.bind("<FocusOut>", lambda event, e=entry, 
+#                    p=placeholder:self.on_focusout(event, e, p))
+#         entry.config(fg="grey")
+#         return entry
+
+#     def on_entry_click(self, event, entry, placeholder):
+#         """Show the text they input."""
+#         if entry.get()==placeholder:
+#             entry.delete(0, "end")
+#             entry.config(fg="black")
+
+#     def on_focusout(self, event, entry, placeholder):
+#         """The colour of what the entry line is for."""
+#         if entry.get()=="":
+#             entry.insert(0, placeholder)
+#             entry.config(fg="grey")
+
+#     def open_dashboard(self):
+#         """Open the dashboard window when all completed."""
+#         self.dashboard=DashboardWindow(self.parent.master, 
+#                                          self.parent.pending_user_info)
+
+
+#     # Saving the data if all the entries is correctly inputted
+#     def signup(self):
+#         """Saving the data and telling the user that."""
+#         if not self.validate_fields():
+#             return
+        
+#         with open(f"{username}.txt", "w") as file:
+#             file.write(f"First Name: {first_name}\n")
+
+#         mb.showinfo("Success", "Your data has been saved. Please login"+
+#                         " next time with your exact details. Thank you :)", 
+#                         parent=self.question_window)
+#         self.question_window.destroy()
+#         self.open_dashboard()
+
 # All the code for the login window
 class LoginWindow:
     """The code for the login window."""
@@ -601,7 +690,7 @@ class DashboardWindow:
             ("HOME", self.display_home),
             ("USER\nMANUAL", self.display_manual),
             ("QUIZ", self.display_quiz),
-            ("LEARN", self.display_success_stories),
+            # ("LEARN", self.display_success_stories),
             ("ABOUT", self.display_about),
             ("HELP", self.display_help)
         ]
@@ -692,6 +781,13 @@ class DashboardWindow:
 
     # The Home page of the Dashboard Window
     def display_help(self):
+        """Displaying the help questions and answer of the program and 
+        picture.
+        """
+        self.clear_main_area()
+
+    # The About page of the Dashboard Window
+    def display_about(self):
         """Displaying the help questions and answer of the program and 
         picture.
         """
